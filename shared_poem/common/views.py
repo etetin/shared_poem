de3ac97f7d9fbf6256d3f0c462398822a9457792
Eugene <e.tetin.another@gmail.com>
2019-11-12 22:19:31 +0300

    Minor fixes.

--- shared_poem/common/views.py
+++ shared_poem/common/views.py
@@ -6,7 +6,8 @@ def index(request):
     _redis = get_redis_connection("default")
     poem = _redis.get('poem')
     if poem is None:
-        poem = _redis.set('poem', 'I v nachale bilo slovo: ')
+        _redis.set('poem', 'I v nachale bilo slovo: ')
+        poem = _redis.get('poem')
 
     args = {
         'poem': poem.decode("utf-8")
